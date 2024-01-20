7710e2bed3c0bf986377e57d55660de7c7c12eb8
JustAbusiness <huysanti123456@gmail.com>
2024-01-20 13:23:35 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 const nextConfig = {
-  // output: "export",
+  output:'static',
   typescript: {
     ignoreBuildErrors: true,
-
   },
-  trailingSlash: true,
   reactStrictMode: true,
   images: {
     domains: ["crealink-images.s3.ap-southeast-1.amazonaws.com",]
