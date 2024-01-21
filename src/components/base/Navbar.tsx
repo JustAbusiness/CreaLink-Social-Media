c7d69fe4c46d4210bb6970f384f9b97b8db52dca
JustAbusiness <huysanti123456@gmail.com>
2024-01-21 15:24:37 +0700

    fix image

--- src/components/base/Navbar.tsx
+++ src/components/base/Navbar.tsx
@@ -12,12 +12,12 @@ import { useSession } from 'next-auth/react';
 
 function Navbar() {
   const { data: session } = useSession();
-  const icons = [HomeOutlined, SearchOutlined, HeartOutlined, BellOutlined, UserOutlined];
+  const antIcons = [HomeOutlined, SearchOutlined, HeartOutlined, BellOutlined, UserOutlined];
   const hrefs = ['/home', '/search', '/favorites', '/notifications', '/profile'];
 
   // const hasNotifications = session?.user?.hasNotification;
   const [hasNotifications, setHasNotifications] = useState(false);
-  console.log("HAS NOTIFICATION: ", hasNotifications);
+  console.log('HAS NOTIFICATION: ', hasNotifications);
   const fetchNotifications = async () => {
     if (!session) {
       return;
@@ -53,24 +53,22 @@ function Navbar() {
     const pollingInterval = setInterval(() => {
       fetchNotifications();
     }, 5000);
-  
+
     // Cleanup interval on component unmount
     return () => clearInterval(pollingInterval);
   }, [session]);
-  
 
-  
   return (
     <div className="w-[600px] ml-[13%] flex justify-center space-x-20">
-      {icons.map((icon, index) => (
+      {antIcons.map((AntIcon, index) => (
         <Link key={index} href={hrefs[index]} passHref>
           <div className="flex flex-col items-center cursor-pointer">
-            <Image src={icon} alt={`Icon ${index + 1}`} width={30} height={30} />
+            <AntIcon style={{ fontSize: '24px' }} />
             {index === 3 && hasNotifications && (
-             <span className="relative flex h-3 w-3 bottom-9 left-1  ">
-             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-700 opacity-75"></span>
-             <span className="absolute inline-flex rounded-full h-3 w-3 bg-red-800"></span>
-           </span>
+              <span className="relative flex h-3 w-3 bottom-9 left-1">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-700 opacity-75"></span>
+                <span className="absolute inline-flex rounded-full h-3 w-3 bg-red-800"></span>
+              </span>
             )}
           </div>
         </Link>
